3d45addd9e9134eeab2cdd3c5e7e841084b8e06a
Athos Carvalho <athosvcc@gmail.com>
2021-02-23 02:58:25 -0300

    Run files for the model

---
+++ runfiles/runGUI - Copia.bat
@@ -0,0 +1,4 @@
+cd ..\NESFIOneStock\classes
+java -cp  .;d:/repast/lib/repast.jar;d:/repast/lib/colt.jar;d:/repast/lib/jgl3.1.0.jar;d:/repast/lib/plot.jar;d:/repast/lib/jmf.jar uchicago.src.sim.engine.SimInit ASMModel.AsmModel 
+cd ..\NESFIOneStock
+pause
---
+++ runfiles/runbatch.bat
@@ -0,0 +1,3 @@
+cd..\NESFIOneStock\classes
+java -cp  .;d:/repast/lib/repast.jar;d:/repast/lib/colt.jar;d:/repast/lib/jgl3.1.0.jar;d:/repast/lib/plot.jar;d:/repast/lib/jmf.jar ASMModel.AsmModel ..\asm.pf 
+cd ..
